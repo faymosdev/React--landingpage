64dd377e5c4be0b3d5b5a5a6bbc258c0180561cc
Your Name <favouromos002@gmail.com>
2025-06-02 11:53:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Landing from './pages/landing'
+import About from './pages/about'
+import Blog from './pages/blog'
+import Contact from './pages/contact'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
